2156b59d3c673d5047f6a005e7c02308c41a769d
Thegodlessman <luischang16@hotmail.com>
2024-11-09 01:32:31 -0400

    Feat: Se ha agregado el redireccionamiento de register y login

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -3,10 +3,13 @@ import axios from 'axios';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonText } from '@ionic/react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useHistory } from 'react-router-dom'; // Importa useHistory
 
 const Login: React.FC = () => {
     const [loginValue, setLoginValue] = useState(''); 
     const [password, setPassword] = useState('');
+
+    const history = useHistory();
 
     const handleLogin = async () => {
         try {
@@ -20,6 +23,11 @@ const Login: React.FC = () => {
             toast.success('Inicio de sesión exitoso', {
                 position: "bottom-center"
             });
+
+            setTimeout(() => {
+                history.push('/home'); // Redirige a la página de login
+            }, 3000); // 3000 ms = 3 segundos
+
         } catch (error: any) {
             toast.error('Error al iniciar sesión', {
                 position: "bottom-center"
